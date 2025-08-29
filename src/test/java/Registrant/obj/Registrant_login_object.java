baf71cb50afa5364259b55ab57a333dddd59d596
VenkateshUdaru <VenkateshUdaru@IKCON1017>
2025-08-29 11:52:07 +0530

    sdfghj

--- src/test/java/Registrant/obj/Registrant_login_object.java
+++ src/test/java/Registrant/obj/Registrant_login_object.java
@@ -5,13 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import Configuration.ConfigReader;
 import Configuration.ConfigWriter;
